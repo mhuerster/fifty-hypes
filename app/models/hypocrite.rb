e2544c80aa4bd1b081da1c788a39de5fedf52f2b
Andrew Goldstein <goldstein@urx.com>
2015-03-08 14:30:39 -0700

    Cleaned up new hypocrite page, added validations, state choice

--- app/models/hypocrite.rb
+++ app/models/hypocrite.rb
@@ -7,7 +7,12 @@ class Hypocrite
   field :twitter, type: String
   field :description, type: String
   field :state_id, type: String
-
+  validates :name, presence: true
+  validates :current_position, presence: true
+  #validates :image_path, presence: true
+  validates :descent, presence: true
+  validates :twitter, presence: true
+  #validates :description, presence: true
+  #validates :state_id, presence: true
   attr_accessible :name, :current_position, :image_path, :descent, :twitter, :description, :state_id
-
 end
