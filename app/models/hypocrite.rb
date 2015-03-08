91bbe16b0839460bcc29ff334d445e532e2c1ab5
Andrew Goldstein <goldstein@urx.com>
2015-03-08 10:44:54 -0700

    Updated fields in form

--- app/models/hypocrite.rb
+++ app/models/hypocrite.rb
@@ -2,12 +2,12 @@ class Hypocrite
   include Mongoid::Document
   field :name, type: String
   field :current_position, type: String
-  field :descent, type: String
-  field :twitter_handle, type: String
   field :image_path, type: String
+  field :descent, type: String
+  field :twitter, type: String
   field :description, type: String
   field :state_id, type: String
 
-  attr_accessible :name, :current_position, :image_path, :description, :state_id, :descent, :twitter_handle
+  attr_accessible :name, :current_position, :image_path, :descent, :twitter, :description, :state_id
 
 end
