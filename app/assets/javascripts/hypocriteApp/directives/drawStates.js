253d2a43f3973b5b01a20006772fe019635d1a63
Andrew <yu.drew@gmail.com>
2015-03-09 12:34:37 -0700

    twitter functionality implemented, some styling

--- app/assets/javascripts/hypocriteApp/directives/drawStates.js
+++ app/assets/javascripts/hypocriteApp/directives/drawStates.js
@@ -12,68 +12,87 @@ hypocriteApp.directive('drawStates', function () {
     
     // templateUrl: "assets/hypocriteApp/templates/states.html",
     
-    link: function ($scope, element, attrs) {		
-  		
-  		$scope.open = false;
+    link: function ($scope, element, attrs) {   
+      
+      $scope.open          = false;
+      $scope.twitterLink   = "<a href='https://twitter.com/intent/tweet?screen_name=50hypocrites' class='twitter-mention-button'>Tweet to @50hypocrites</a>";
+      $scope.twitterScript = "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
 
       $scope.generateModal = function(stateData) {
 
-        return   "<div ng-hide='hidden'>"+"<h4>"+(stateData.peo)+"</h4><img src='"+(stateData.image)+"' style='width:200px'>"+"<table>"+
-          "<tr><td>Profession:</td><td>"+(stateData.prof)+"</td></tr>"+
-          "<tr><td>State:</td><td>"+(stateData.stateName)+"</td></tr>"+
-          "<tr><td>Heritage:</td><td>"+(stateData.desc)+"</td></tr></table>"+
-          "<a href='https://twitter.com/intent/tweet?screen_name=goldstein_andr' class='twitter-mention-button' data-related='goldstein_andr'>Tweet to @DanaRohrabacher </a>"+
-            "<a href='https://twitter.com/intent/tweet?screen_name=50hypocrites' class='twitter-mention-button'>Tweet to @50hypocrites</a>"+ 
-            "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>"+"</div>"
+        return   "<div id='state-modal-container'>"+
+          "<h4>"+(stateData.peo)+"</h4><img src='"+(stateData.image)+"' style='width:200px'>"+
+          "<table>"+
+            "<tr><td>Profession:</td><td>"+(stateData.prof)+"</td></tr>"+
+            "<tr><td>State:</td><td>"+(stateData.stateName)+"</td></tr>"+
+            "<tr><td>Heritage:</td><td>"+(stateData.desc)+"</td></tr>"+
+          "</table>"+ 
+          $scope.twitterLink +
+          $scope.twitterScript +
+          "</div>"
+
         }
 
-  		$scope.drawMap = function(mapId, data){
-  			
+      $scope.drawMap = function(mapId, data){
+        
         function showModal(stateData){
 
-          var hypStateData = data[stateData.id]; // p.id is state acronym/id
+          var hypStateData = data[stateData.id];
 
-  				d3.select("#state-modal").transition().duration(200).style("opacity", .99);      
-  				d3.select("#state-modal").html($scope.generateModal(hypStateData))  
-          .style("left", (d3.event.pageX) - 120 + "px")     
-          .style("top", (d3.event.pageY - 180) + "px");          
-  			}
-  			
-  			function hideModal(){
+          d3.select("#state-modal").transition().duration(200).style("opacity", .99);             
+          $("#state-modal").html($scope.generateModal(hypStateData));  
+          d3.select("#state-modal").style("left", (d3.event.pageX) - 120 + "px").style("top", (d3.event.pageY - 180) + "px");      
+          $scope.open = true;
+        }
+        
+        function hideModal(){
           console.log("hide modal invoked")
-  				d3.select("#state-modal").transition().duration(200).style("opacity", 0);      
-  			}	
+          d3.select("#state-modal").transition().duration(200).style("opacity", 0);      
+          $scope.open = false;
+        } 
 
-  			function toggleModal(stateData) {
+        function toggleModal(stateData) {
 
           if($scope.open) {
-  					hideModal();
-  					$scope.open = false;
-  				} else {
-  					showModal(stateData);
-  					$scope.open = true;
-  				}
-  			}
+            hideModal();
+          } else {
+            showModal(stateData);
+          }
+        }
+        
 
-  			d3.select(mapId).selectAll(".state")
-  				.data($scope.stateBoundaries)
-  				.enter().append("path")
-  				.attr("class","state")
+        d3.select(mapId).selectAll(".state")
+          .data($scope.stateBoundaries)
+          .enter().append("path")
+          .attr("class","state")
           .attr("d", 
             function(stateData){ 
               return stateData.d;
           })
-  				.style("fill",function(stateData){ 
+          .style("fill",function(stateData){ 
             return $scope.data[stateData.id].color; 
           })
           .on("click", toggleModal);
-  		}
+      
+        // Click outside map to close modal
+
+        // document.getElementById('outer-container').onclick = function(e) {
+        //   if(e.target == document.getElementById('outer-container')) {
+        //     console.log("Clicked outside")  
+        //     hideModal();  
+        //   }
+        //   else {
+        //     console.log("Didnt click outside")    
+        //   }
+        // }
+      }
 
       // Invoke drawMap() to create the map with data populated in $scope.data
       $scope.drawMap("#statesvg", $scope.data);
 
-      $scope.$watch('exp', function (newVal, oldVal) {
-      });
+      // $watch function to potentially use later
+      // $scope.$watch('exp', function (newVal, oldVal) {
+      // });
     }
   };
 });
